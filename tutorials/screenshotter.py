430563bb6291d768ba07cd96f399e2884e6b3826
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-05-06 16:52:48 -0500

    Updated screenshotter

--- tutorials/screenshotter.py
+++ tutorials/screenshotter.py
@@ -1,5 +1,13 @@
-import sys # Imports necessary modules
+import sys # Import modules
 from playwright.sync_api import sync_playwright
 
 def run(playwright, url, take_screenshot):
     browser = playwright.chromium.launch() # Launch Chromium headless browser
+    page = browser.new_page()
+    page.goto(url) # Proceed to requested page
+
+    if take_screenshot: # Save page screenshot as PNG if requested
+        page.screenshot(path="screenshot.png", full_page=True) # full_page set to True to take care of long pages with scrolling
+        print("Screenshot saved as screenshot.png")
+    
+    browser.close() # Close browser
