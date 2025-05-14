ca998d6455a7a8b2cb1f2a2cf5a634ba3682a779
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-05-14 12:00:42 -0500

    Added pyproject.toml and created sitemap scanner

---
+++ tutorials/sitemapScanner.py
@@ -0,0 +1,47 @@
+# Scans sitemap of website and prepares CSV file with all found links
+
+import requests
+from bs4 import Beautiful as Soup
+import os
+import csv
+
+ATTRS = ["loc", "lastmod", "priority"] # Constants for attributes to be extracted from sitemap
+
+def parse_sitemap(url, csv_filename="urls.csv"):
+    """Parse the sitemap at the given URL and append the data to a CSV file."""
+
+    if not url: # If URL is not provided
+        return False
+    
+    response = requests.get(url) # Attempt to get content from URL
+
+    if response.status_code != 200: # If response status is not 200 (ok)
+        return False
+    
+    soup = Soup(response.content, "xml") # Parse XML content of response
+
+    for sitemap in soup.find_all("sitemap"): # Recursively parse nested sitemaps
+        loc = sitemap.find("loc").text
+        parse_sitemap(loc, csv_filename)
+    
+    root = os.path.dirname(os.path.abspath(__file__)) # Define root dir for saving CSV file
+
+    urls = soup.find_all("url") # Find all URL entries in sitemap
+
+    rows = []
+    for url in urls:
+        row = []
+        for attr in ATTRS:
+            found_attr = url.find(attr)
+            row.append(found_attr.text if found_attr else "n/a") # Use n/a if attribute not found, otherwise get its text
+        rows.append(row)
+    
+    file_exists = os.path.isfile(os.path.join(root, csv_filename)) # Check if file already exists
+
+    with open(os.path.join(root, csv_filename), "a+", newline="") as csvfile: # Append data to CSV file
+        write = csv.write(csvfile)
+        if not file_exists: # Write header only if file doesn't exist
+            writer.writerow(ATTRS)
+        writer.writerows(rows)
+
+parse_sitemap("") # Insert URL
